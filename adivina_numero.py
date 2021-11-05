1f2b3044003c17d780365012ce739b75d32c86ca
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2021-11-05 09:45:21 +0100

    cambio

--- adivina_numero.py
+++ adivina_numero.py
@@ -2,15 +2,15 @@ import random
 numero = random.randint(0,100)
 print(numero)
 print("Intente con un número entero.")
-intento = input()
-while str(intento) != str(numero):
-    if str(intento) > str(99):
+intento = int(input())
+while intento != numero:
+    if intento > 99:
         print("El número está entre 0 y 99. Elige otro.")
     else:
-        if str(intento) > str(numero):
-            print("Te has pasado. El numero está entre 0 y " + str(intento) + ".")
+        if intento > numero:
+            print("Te has pasado.")
         else:
-            print("Te has quedado corto. El numero está entre " + str(intento) + " y 99.")
-    intento = input()
-if str(intento) == str(numero):
+            print("Te has quedado corto.")
+    intento = int(input())
+if intento == numero:
      print("¡Felicidades, has acertado!")
